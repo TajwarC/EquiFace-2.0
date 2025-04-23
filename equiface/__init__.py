146d321cebdea5cb049623df363be56ede9bfd6e
TajwarC <tajwarc96@gmail.com>
2025-04-23 09:11:19 +0100

    edit __init__.py

--- equiface/__init__.py
+++ equiface/__init__.py
@@ -1,11 +1,14 @@
-from .verification import FPR, FNR
+from .verification import FPR as verification_FPR, FNR as verification_FNR
+from .deepface import FPR as deepface_FPR, FNR as deepface_FNR
 from .image_utils import preprocess_image, get_embedding
 from .logging_utils import log_results
 from .constants import SUPPORTED_EXTENSIONS, LOG_FILE
 
 __all__ = [
-    "FPR",
-    "FNR",
+    "verification_FPR",
+    "verification_FNR",
+    "deepface_FPR",
+    "deepface_FNR",
     "preprocess_image",
     "get_embedding",
     "log_results",
